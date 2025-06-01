fa34c9e240d44416fc245e15e88823273a70f71f
AmartiSamia <samia.elamarti11@gmail.com>
2025-06-01 01:45:28 +0100

    final commit

--- obj/Debug/net8.0/Project.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b8fa5c2da523be8ca9097a728eb52a8876d47d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb30565fa0473a50ce5aecf2ab6475ac4221131")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
